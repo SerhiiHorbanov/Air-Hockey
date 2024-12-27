70c610eb8902e2e990881aff07d1ca237e2e0e21
Serhii <serikgorbanev@gmail.com>
2024-12-27 15:59:20 +0100

    Removed some code duplication for collision resolution methods

--- Circle.cs
+++ Circle.cs
@@ -32,43 +32,46 @@ class Circle
 
     public void CheckAndResolveCollision(Circle other)
     {
-        float distanceSquared = _position.DistanceSquaredTo(other._position);
+        float distanceSquaredToCenter = _position.DistanceSquaredTo(other._position);
         float radiusesSum = _radius + other._radius;
         
-        if (distanceSquared > radiusesSum * radiusesSum)
+        if (distanceSquaredToCenter > radiusesSum * radiusesSum)
             return;
         
-        float distance = float.Sqrt(distanceSquared);
-        
-        Vector2f collisionNormal = (other._position - _position) / distance;
+        float distanceToCenter = float.Sqrt(distanceSquaredToCenter);
 
-        float overlapDepth = distance - radiusesSum;
-        _position += collisionNormal * overlapDepth;
+        Vector2f collisionPoint = (_position - other._position) / distanceToCenter;
+        collisionPoint *= other._radius;
+        collisionPoint += other._position;
+        float distanceToCollisionPoint = distanceToCenter - other._radius;
         
-        float force = other.Velocity.Dot(collisionNormal) - Velocity.Dot(collisionNormal);
+        ResolveCollisionWithPoint(collisionPoint, other.Velocity, distanceToCollisionPoint);
+    }
+
+    private void ResolveCollisionWithPoint(Vector2f point, Vector2f velocity, float distance)
+    {
+        Vector2f collisionNormal = (point - _position) / distance;
+
+        float overlapDepth = distance - _radius;
+        _position += collisionNormal * overlapDepth;
+
+        float force = velocity.Dot(collisionNormal) - Velocity.Dot(collisionNormal);
         
         Velocity += collisionNormal * force;
-        other.Velocity += collisionNormal * -force;
     }
+
+    private void ResolveCollisionWithPoint(Vector2f point, float distance)
+        => ResolveCollisionWithPoint(point, new(0, 0), distance);
     
     public void CheckAndResolveCollision(Rectangle rectangle)
     {
         Vector2f closestPoint = rectangle.GetClosestPointTo(_position);
-        Console.WriteLine(_position.ToString());
         float distanceSquared = _position.DistanceSquaredTo(closestPoint);
         
         if (distanceSquared > _radius * _radius)
             return;
-            
+
         float distance = float.Sqrt(distanceSquared);
-        
-        Vector2f collisionNormal = (closestPoint - _position) / distance;
-        
-        float overlapDepth = distance - _radius;
-        _position += collisionNormal * overlapDepth;
-        
-        float force = -Velocity.Dot(collisionNormal) * 2;
-        
-        Velocity += collisionNormal * force;
+        ResolveCollisionWithPoint(closestPoint, distance);
     }
 }
