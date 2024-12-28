1ac06a04a4f91e1cb1f43555af444cc7fad79fc1
Serhii <serikgorbanev@gmail.com>
2024-12-28 07:42:49 +0100

    Added GetClosestPointOnPerimeter to Rectangle

--- Rectangle.cs
+++ Rectangle.cs
@@ -6,6 +6,15 @@ class Rectangle
     private FloatRect _rect;
     private RectangleShape _shape;
 
+    public float Left
+        => _rect.Left;
+    public float Right
+        => _rect.Left + _rect.Width;
+    public float Top
+        => _rect.Top;
+    public float Bottom
+        => _rect.Top + _rect.Height;
+    
     public Rectangle(FloatRect rect, Color color)
     {
         _rect = rect;
@@ -22,4 +31,24 @@ class Rectangle
 
     public Vector2f GetClosestPointTo(Vector2f position)
         => VectorMath.Clamp(position, _rect);
+
+    bool IsXInside(float x)
+        => x > Left && x < Right;
+    bool IsYInside(float y)
+        => y > Top && y < Bottom;
+    bool IsInside(Vector2f point)
+        => IsXInside(point.X) && IsYInside(point.Y);
+    
+    public Vector2f GetClosestPointOnPerimeter(Vector2f position)
+    {
+        if (!IsInside(position))
+            return GetClosestPointTo(position);
+        
+        Vector2f closestOnLeft = new(Left, position.Y);
+        Vector2f closestOnRight = new(Right, position.Y);
+        Vector2f closestOnTop = new(position.X, Top);
+        Vector2f closestOnBottom = new(position.X, Bottom);
+        
+        return position.GetClosestPoint(closestOnLeft, closestOnRight, closestOnTop, closestOnBottom);
+    }
 }
