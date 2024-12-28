08497da11c9c762d80504ec28ae6e2a6ec88598c
Serhii <serikgorbanev@gmail.com>
2024-12-28 03:11:25 +0100

    Added DistanceTo, GetClosestPoint, Lerp to VectorMath

--- VectorMath.cs
+++ VectorMath.cs
@@ -10,6 +10,9 @@ static class VectorMath
         return (diff.X * diff.X) + (diff.Y * diff.Y);
     }
 
+    public static float DistanceTo(this Vector2f first, Vector2f second)
+        => float.Sqrt(first.DistanceSquaredTo(second));
+    
     public static float Dot(this Vector2f first, Vector2f second)
     {
         return (first.X * second.X) + (first.Y * second.Y);
@@ -28,4 +31,24 @@ static class VectorMath
             vector.Y = rect.Top + rect.Height;
         return vector;
     }
+
+    public static Vector2f GetClosestPoint(this Vector2f point, params Vector2f[] points)
+    {
+        Vector2f closest = points[0];
+        float closestDistanceSquared = point.DistanceSquaredTo(closest);
+        foreach (Vector2f current in points.Skip(1))
+        {
+            float currentDistanceSquared = point.DistanceSquaredTo(current);
+            if (currentDistanceSquared < closestDistanceSquared)
+            {
+                closestDistanceSquared = currentDistanceSquared;
+                closest = current;
+            }
+        }
+
+        return closest;
+    }
+
+    public static Vector2f Lerp(this Vector2f interpolated, Vector2f interpolatingTo, float interpolation)
+        => interpolated + ((interpolatingTo - interpolated) * interpolation);
 }
